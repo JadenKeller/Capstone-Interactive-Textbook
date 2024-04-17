6396952086177920c52282ac09ea31252f9b451a
Luke Moore <lukemoore237@gmail.com>
2024-04-17 13:56:25 -0700

    Lambertian lighting content.

--- src/pages/lessons/vector_operations/index.tsx
+++ src/pages/lessons/vector_operations/index.tsx
@@ -20,8 +20,8 @@ export default function VectorOpChapter() {
 						</h2>
 						<p>
 							The dot product, also known as the scalar product, is a mathematical operation that takes two equal-length (normalized) vectors and returns a single number.
-							This resulting value can be used to interpret the relationship between objects and angles in a scene depending on the context.
-							Just to name a few, some example use cases for the dot product are calculating the angle between two vectors, projecting a vector onto another, useful for shadow mapping and projections of 3D objects onto surfaces or screens.
+							This resulting value can be used to interpret the relationship between angles in a scene and is used for multiple situations.
+							Just to name a few examples, the dot product is used to calculate the angle between two vectors, projecting a vector onto another, and is useful for shadow mapping and projections of 3D objects onto surfaces or screens.
 							They are also used to calculate surface normals and detecting collisions between objects or detecting whether they are facing the same direction or not.
 							We go over the latter in the first activity of this lesson.
 						</p>
@@ -33,7 +33,7 @@ export default function VectorOpChapter() {
 							If the result is zero, it means the vectors are orthogonal to each other.
 						</p>
 						<p>
-							A "landmark value" is a dot product of two vectors that equals one, zero, or negative one.
+							A "landmark value" is a dot product result that equals one, zero, or negative one.
 							These values are significant because they represent different relationships between the directions of the two vectors.
 							A value of one means that the directions are entirely parallel, or facing the same direction.
 							A value of negative one means the opposite, that the two vectors are non-parallel.
@@ -53,15 +53,20 @@ export default function VectorOpChapter() {
 							<h2>Lambertian Lighting</h2>
 							<p>
 								Lambertian lighting is a model used for simulating diffuse reflection from surfaces in which vector operations like the dot product and normalization play a curcial role.
-								In this activity, we demonstrate the relationship between the angles of the direction of light and a surface normal vector.
-								
+								In this activity, we demonstrate the relationship between these angles and how the dot product is used to calculate the intensity of diffused light.
+							</p>
+							<h3>More Dot Product</h3>
+							<p>
+								One helpful interpretation of the dot product we haven't mentioned previously is that it can be used to find the cosine of the angle between two vectors.
+								The cosine of the surface normal and the incoming light vector reveals how much light the point receives.
+								In other words, the intensity of the light on a surface is proportional to the cosine of the angle between the surface normal and the direction of the incoming light.
+								Our landmark values are helpful for interpreting this result too. A value of one means the point is in direct lighting, and a value of zero or less means that point is in shadow.
+								In the activity below, this relationship between surface normals and lighting vectors is demonstrated to calculate the diffuse lighting on a sphere.
+								What happens when the angle between these vectors changes?
 							</p>
 						</section>
 					}
-					canvas={<PlusMinus />}
-				/>
-
-			{/* Chapter navigation buttons */}
+					canvas={<PlusMinus />} />
 		</div>
 	)
 }
