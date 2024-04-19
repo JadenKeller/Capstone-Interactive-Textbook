a5357c2b75bb841f8da65e37797b1f06f2fec094
ConlynPattison <cpattison@csumb.edu>
2024-04-19 16:25:56 -0700

    Added structure and content to both transformations in widget

--- src/components/lessons/vectors/BasisTransformations/BasisTransformations.tsx
+++ src/components/lessons/vectors/BasisTransformations/BasisTransformations.tsx
@@ -3,7 +3,32 @@ import styles from "./BasisTransformations.module.css";
 export default function BasisTransformations() {
 	return (
 		<div className={styles.widget}>
-			hello
+			<h3>
+				Rotation
+			</h3>
+			<div>
+				<label><input className={styles.num_input} type="number" /> degrees</label>
+			</div>
+			<div>
+				About
+				<input type="radio" name="axis" />x
+				<input type="radio" name="axis" />y
+				<input type="radio" name="axis" />z
+			</div>
+			<button className={styles.button} type="button">Apply</button>
+			<h3>
+				Scale
+			</h3>
+			<div>
+				<label>Scalar <input className={styles.num_input} type="number" /></label>
+			</div>
+			<div>
+				Scaling on
+				<input type="radio" name="axis" />x
+				<input type="radio" name="axis" />y
+				<input type="radio" name="axis" />z
+			</div>
+			<button className={styles.button} type="button">Apply</button>
 		</div>
 	);
 }
