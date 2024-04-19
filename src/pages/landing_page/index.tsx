6c6f90e9a48b3919201647a63dbdf9b16f2d8426
Luke Moore <lukemoore237@gmail.com>
2024-04-19 15:53:47 -0700

    Started landing page.

--- src/pages/landing_page/index.tsx
+++ src/pages/landing_page/index.tsx
@@ -3,8 +3,17 @@ import styles from "./index.module.css";
 
 export default function LandingPage() {
     return (
-            <div className={styles.body_root}>
-                    <h1>Interactive Textbook for Linear Algebr</h1>
-            </div>
+		<div className={styles.body_root}>
+
+			<div className={styles.banner_section}>
+				<h1>Interactive Textbook for Geometric Linear Algebra</h1>
+			</div>
+
+			<div className={styles.call_to_action}>
+				<h3>Explore math concepts behind computer graphics through play & activities</h3>
+			</div>
+
+
+		</div>
     );
 }
