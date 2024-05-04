7b39f12f91398d74c4accbb0f3ef83d8ffc2764d
Luke Moore <lukemoore237@gmail.com>
2024-05-04 15:26:34 -0700

    Added dropdown window for lesson navigation.

--- src/components/ui/Nav/Nav.tsx
+++ src/components/ui/Nav/Nav.tsx
@@ -2,17 +2,27 @@ import styles from "./Nav.module.css";
 import { useNavigate } from "react-router-dom";
 import { TransformationStateManager } from "@components/InteractiveCanvas/InteractiveCanvas";
 
-
-/**
- * 
- * @todo fix me: this should really be a nav component, not a header
- */
 export default function Nav() {
+	const nav = useNavigate();
+	const navigate = (fullPath: string) => {
+		TransformationStateManager.clear();
+		nav(fullPath);
+	}
+
 	return (
-		<section className={styles.nav_bar}>
-			<div className={`${styles.lessons_button} ${styles.nav_button}`}>Lessons</div>
-			<div className={`${styles.home_button} ${styles.nav_button}`}>Home</div>
-			<div className={`${styles.logo}`}><img src="/logo-icon.svg" height={"65px"} width={"70px"}></img></div>
-		</section>
-	)
+		<nav className={styles.nav_bar} role="navigation">
+			<ul className={styles.nav_bar}>
+				<li className={`${styles.lessons_button} ${styles.nav_button}`}>
+					Lessons
+					<ul className={styles.nav_dropdown_content}>
+						<li className={styles.nav_button} onClick={() => navigate("/lesson/vectors")}>Vectors</li>
+						<li className={styles.nav_button} onClick={() => navigate("/lesson/vector-operations")}>Vector Operations</li>
+						<li className={styles.nav_button} onClick={() => navigate("/lesson/matrices")}>Matrix Transformations</li>
+					</ul>
+				</li>
+				<li className={`${styles.home_button} ${styles.nav_button}`} onClick={() => navigate("/")}>Home</li>
+				<li className={`${styles.logo}`}><img src="/logo-icon.svg" height={"65px"} width={"70px"}></img></li>
+			</ul>
+		</nav>
+	);
 }
